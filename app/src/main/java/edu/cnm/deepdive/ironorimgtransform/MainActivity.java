eaabbd43d0e4adc2f2dd9bfcc43ff5af426c4737
Todd A Nordquist <toddnordquist144@gmail.com>
2019-02-22 09:59:27 -0700

    Began process of laying out view.

--- app/src/main/java/edu/cnm/deepdive/ironorimgtransform/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/ironorimgtransform/MainActivity.java
@@ -2,12 +2,32 @@ package edu.cnm.deepdive.ironorimgtransform;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+  private TextView numberView;
+  private ToggleButton transformToggle;
+  private ToggleButton imageToggle;
+  private TextView transformView;
+  private TextView imageView;
+  private Random rng;
+  private int value;
+//  private int activeCorrect;
+//  private int passiveCorrect;
+//  private int incorrect;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    numberView = findViewById(R.id.number_view);
+    transformToggle = findViewById(R.id.tranform_toggle);
+    imageToggle = findViewById(R.id.buzz_toggle);
+    transformView = findViewById(R.id.transform_view);
+    imageView = findViewById(R.id.image_view);
+    rng = new Random();
   }
 }
