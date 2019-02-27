193048b61ff19ddc334702f70cdd72e20eef414b
Todd A Nordquist <toddnordquist144@gmail.com>
2019-02-27 11:09:32 -0700

    Added version 2 of simple ERD.

--- app/src/main/java/edu/cnm/deepdive/ironorimgtransform/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/ironorimgtransform/MainActivity.java
@@ -23,11 +23,31 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    numberView = findViewById(R.id.number_view);
-    transformToggle = findViewById(R.id.tranform_toggle);
-    imageToggle = findViewById(R.id.buzz_toggle);
+    //numberView = findViewById(R.id.number_view);
+    transformToggle = findViewById(R.id.transform_toggle);
+    imageToggle = findViewById(R.id.image_toggle);
     transformView = findViewById(R.id.transform_view);
-    imageView = findViewById(R.id.image_view);
+    imageView = findViewById(R.id.current_view);
     rng = new Random();
+    updateTallyDisplay();
   }
+
+  private void updateTallyDisplay() {
+    imageView.setText(getString(R.string.current_image));
+    transformView.setText(getString(R.string.transform_name));
+  }
+
+//  private void updateView() {
+//    value = 1 + rng.nextInt(UPPER_BOUND);
+//    runOnUiThread(new Runnable() {
+//      @Override
+//      public void run() {
+//        updateTallyDisplay();
+//        numberView.setText(Integer.toString(value));
+//        fizzToggle.setChecked(false);
+//        buzzToggle.setChecked(false);
+//      }
+//    });
+//  }
+
 }
